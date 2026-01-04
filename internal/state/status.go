b41835683f97e18ae7ebd94f7b921203cda1b164
Austin Jones <ajones53.aj@gmail.com>
2026-01-04 18:52:11 -0500

    yagni

--- internal/state/status.go
+++ internal/state/status.go
@@ -2,9 +2,7 @@ package state
 
 import (
 	"database/sql"
-	"encoding/json"
 	"errors"
-	"os"
 
 	"github.com/ajone239/nameplate/internal/models"
 )
@@ -85,44 +83,4 @@ func (s *SqliteStatusStore) SetStatus(status *models.Status) error {
 	}
 
 	return nil
-}
-
-type JsonStatusStore struct {
-	path string
-}
-
-func (j *JsonStatusStore) InitStore() (bool, error) {
-	return true, nil
-}
-
-func (j *JsonStatusStore) load() (*models.Status, error) {
-	data, err := os.ReadFile(j.path)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return &models.Status{}, nil
-		}
-		return nil, err
-	}
-
-	var status models.Status
-	json.Unmarshal(data, &status)
-
-	return &status, nil
-}
-
-func (j *JsonStatusStore) save(s *models.Status) error {
-	data, err := json.MarshalIndent(s, "", "    ")
-	if err != nil {
-		return err
-	}
-
-	return os.WriteFile(j.path, data, 0644)
-}
-
-func (j *JsonStatusStore) GetStatus() (*models.Status, error) {
-	return j.load()
-}
-
-func (j *JsonStatusStore) SetStatus(s *models.Status) error {
-	return j.save(s)
 }
